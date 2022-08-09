e0d2206a8fb62fc5bcff95ad0fa640be33b08631
renee <27038339+reneebot@users.noreply.github.com>
2022-08-09 08:25:16 -0500

    Create macosdefaults.sh

---
+++ macosdefaults.sh
@@ -0,0 +1,5 @@
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "$HOME/Dropbox/Screenshots"
