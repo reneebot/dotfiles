07894a1dce3d989dcc99a4e8554417d474170dac
renee <27038339+reneebot@users.noreply.github.com>
2022-08-09 08:22:34 -0500

    Create homebrew.sh

---
+++ homebrew.sh
@@ -0,0 +1,14 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask appcleaner
+brew install --cask audacity
+brew install --cask bettertouchtool
+brew install --cask discord
+brew install --cask dropbox
+brew install --cask ferdi
+brew install --cask google-chrome
+brew install --cask itsycal
+brew install --cask mactex
+brew install --cask musescore
+brew install --cask spotify
+brew install --cask the-unarchiver
