8f906460eef97d56b191a1a993ad48c1a339c869
renee <27038339+reneebot@users.noreply.github.com>
2022-08-27 18:41:43 -0500

    change scroll direction

--- macosdefaults.sh
+++ macosdefaults.sh
@@ -3,3 +3,6 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "$HOME/Dropbox/Screenshots"
+
+# Change scroll direction
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
