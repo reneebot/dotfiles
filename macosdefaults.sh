30b97e5844e7fd1512fb0be8f284a437b4d26530
renee <27038339+reneebot@users.noreply.github.com>
2022-08-27 21:23:56 -0500

    Update macosdefaults.sh

--- macosdefaults.sh
+++ macosdefaults.sh
@@ -1,4 +1,5 @@
-# Finder: show hidden files by default
+# Finder
+# show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
 # show file extensions
@@ -7,10 +8,13 @@ defaults write -g AppleShowAllExtensions -bool true
 # change to column view
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
+# new finder window opens Downloads
+defaults write com.apple.finder NewWindowTarget -string "PfLo"
+defaults write com.apple.finder NewWindowTargetPath -string "file://$HOME/Downloads/"
 killall Finder
 
-# Save screenshots to the Dropbox
-defaults write com.apple.screencapture location -string "$HOME/Dropbox/Screenshots"
+# Save screenshots to Pictures
+defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
 
 # Change scroll direction
 defaults write -g com.apple.swipescrolldirection -bool false
@@ -20,5 +24,19 @@ defaults write com.apple.dock orientation -string "left"
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float "0"
 defaults write com.apple.dock show-recents -bool false
-
 killall Dock
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Disable automatic capitalization as it’s annoying when typing code
+defaults write -g NSAutomaticCapitalizationEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+
+# Disable automatic period substitution as it’s annoying when typing code
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
