fb0605d08c889d2cb3406124fd00ec0e76b6848b
renee <27038339+reneebot@users.noreply.github.com>
2022-08-27 19:43:58 -0500

    Update macosdefaults.sh

--- macosdefaults.sh
+++ macosdefaults.sh
@@ -1,8 +1,24 @@
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Save screenshots to the desktop
+# show file extensions
+defaults write -g AppleShowAllExtensions -bool true
+
+# change to column view
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
+
+killall Finder
+
+# Save screenshots to the Dropbox
 defaults write com.apple.screencapture location -string "$HOME/Dropbox/Screenshots"
 
 # Change scroll direction
 defaults write -g com.apple.swipescrolldirection -bool false
+
+# Dock changes
+defaults write com.apple.dock orientation -string "left"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float "0"
+defaults write com.apple.dock show-recents -bool false
+
+killall Dock
