20d7e4d406d64db94ecafe4bf5927ccb4889b561
renee <27038339+reneebot@users.noreply.github.com>
2022-09-04 22:38:07 -0500

    update homebrew script

---
+++ homebrew-install.sh
@@ -0,0 +1,38 @@
+#!/bin/zsh
+
+echo "Installing Homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Installing apps..."
+
+# Essential apps
+brew install --cask google-chrome
+brew install --cask spotify
+brew install --cask bettertouchtool
+brew install --cask dropbox
+
+# Utilities
+brew install --cask appcleaner
+brew install --cask audacity
+brew install --cask itsycal
+brew install --cask the-unarchiver
+
+# Communication
+brew install --cask discord
+brew install --cask ferdi
+brew install --cask zoom
+
+# Development
+brew install git
+brew install --cask github
+brew install --cask visual-studio-code
+
+# Other
+brew install --cask mactex
+brew install --cask musescore
+brew install --cask runelite
+brew install glfw3
+brew install glew
+
+
+
