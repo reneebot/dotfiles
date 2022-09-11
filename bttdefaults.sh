78e2fec9c5c6043df19d81089c3f7580fb5d725a
renee <27038339+reneebot@users.noreply.github.com>
2022-09-10 21:50:02 -0500

    Update bttdefaults.sh

--- bttdefaults.sh
+++ bttdefaults.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+defaults write com.hegenberg.BetterTouchTool BTTSyncCloudProvider -int 1
+defaults write com.hegenberg.BetterTouchTool BTTDropboxSyncActive -bool true
 defaults write com.hegenberg.BetterTouchTool BTTTouchBarVisible -bool true
 defaults write com.hegenberg.BetterTouchTool BTTShowBTTWhenControlStripHidden -bool false
 defaults write com.hegenberg.BetterTouchTool BTTShowControlStrip -bool false
