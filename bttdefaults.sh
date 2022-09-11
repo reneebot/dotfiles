8993d28fb24e3801fb55ffab8b37001869843309
renee <27038339+reneebot@users.noreply.github.com>
2022-09-10 21:41:10 -0500

    Create bttdefaults.sh

---
+++ bttdefaults.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+defaults write com.hegenberg.BetterTouchTool BTTTouchBarVisible -bool true
+defaults write com.hegenberg.BetterTouchTool BTTShowBTTWhenControlStripHidden -bool false
+defaults write com.hegenberg.BetterTouchTool BTTShowControlStrip -bool false
+defaults write com.hegenberg.BetterTouchTool SUAutomaticallyUpdate -bool true
+defaults write com.hegenberg.BetterTouchTool launchOnStartup -bool true
+defaults write com.hegenberg.BetterTouchTool checkForCrashreports -bool true
